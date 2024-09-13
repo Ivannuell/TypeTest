c6b094b538939e8d1afd4e6569e8ec40fd45a2ef
Ivann <i.herrera.535614@umindanao.edu.ph>
2024-09-13 09:17:27 +0800

    just a layout edit

--- src/App.tsx
+++ src/App.tsx
@@ -25,24 +25,32 @@ function App() {
   return (
     <div className="container mx-auto p-4 flex flex-col items-center space-y-6 bg-gray-900 text-white min-h-screen">
 
-      {!showingResults ? (
-        <Test
-          words={word}
-          input={input}
-          handleInputChange={handleInputChange}
-          timeActive={timeActive}
-          timeLeft={timeLeft}
-          resetTest={resetTest}
-          inputRef={inputRef}
-        />
-      ) : (
-        <Results
-          wpm={wpm}
-          accuracy={accuracy}
-          errors={errorCount}
-          resetTest={retryTest}
-        />
-      )}  
+      {!showingResults ?
+
+        (
+          <Test
+            words={word}
+            input={input}
+            handleInputChange={handleInputChange}
+            timeActive={timeActive}
+            timeLeft={timeLeft}
+            resetTest={resetTest}
+            inputRef={inputRef}
+          />
+        )
+
+        :
+
+        (
+          <Results
+            wpm={wpm}
+            accuracy={accuracy}
+            errors={errorCount}
+            resetTest={retryTest}
+          />
+        )
+
+      }
     </div>
   );
 }
